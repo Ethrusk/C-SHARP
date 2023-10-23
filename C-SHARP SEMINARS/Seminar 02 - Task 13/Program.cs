51998c44b95a1cdac6c9d06a8347ca3eaf2bbf2a
Ethrusk <ethrusk@yandex.ru>
2023-10-23 18:04:56 +0300

    Seminar 02 - Task 13 - changes

--- C-SHARP SEMINARS/Seminar 02 - Task 13/Program.cs
+++ C-SHARP SEMINARS/Seminar 02 - Task 13/Program.cs
@@ -14,21 +14,98 @@
 // Или же делить на 10 без остатка до тех пока результат не окажется в диапазоне от 100 до 999
 // И надо разделить его с остатком на 10, и получим 3ю цифру.
 
-Console.WriteLine("Введите любое число");
+// Console.WriteLine("Введите любое число");
 
-int AnyNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Вы ввели число ");
-Console.Write(AnyNumber);
+// int AnyNumber = Convert.ToInt32(Console.ReadLine());
+// // Console.Write("Вы ввели число ");
+// // Console.Write(AnyNumber);
 
-for (int i = 0; i <= AnyNumber; i++)
-    {
-    int Result = AnyNumber % 10;
+// int number = 0;
+
+// // for (int i = 0; i <= AnyNumber; i++)
+// //     {
+// //     int Result = AnyNumber % 10;
     
-        while (Result > 100 && Result < 1000)
-        {    int ThirdNumber = Result / 10;
-             Console.Write("Третья цифра введённого числа - ");
-             Console.Write(ThirdNumber);
+// //         while (Result > 100 && Result < 999)
+// //         {    int ThirdNumber = Result / 10;
+// //              Console.Write("Третья цифра введённого числа - ");
+// //              Console.Write(ThirdNumber);
+// //         }
+
+
+// //     }
+
+// int ThirdDigit(int AnyNumber)
+//         {
+//             int result = -1;
+//             if (AnyNumber >= 100)
+//             {
+//                 while (number > 999)
+//                 {
+//                     number = number / 10;
+//                 }
+//                 result = number % 10;
+//             }
+//             return result; 
+//         }
+
+// int GetSecond(int k)
+//         {
+//         while (k >= 100) k /= 10;
+//         int d = k % 10;
+//         return d;
+//         }
+//         Console.WriteLine(GetSecond(k));
+
+
+// Console.WriteLine("Введите трехзначное число");
+//         int.TryParse(Console.ReadLine()!, out int k);
+    
+//         if (k>999 || k<100)
+//         {
+//         Console.WriteLine("Не трехзначное число!!!");
+//         return;
+//         }
+//         int GetSecond(int k)
+//         {
+//         while (k >= 100) k /= 10;
+//         int d = k % 10;
+//         return d;
+//         }
+//         Console.WriteLine(GetSecond(k));
+
+
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+
+
+// ФУНКЦИИ------------------------------------------------------------------------------------------------------
+
+// Функция принимает сообщение для отображения в консоли, и выводит результат введенных данных пользователем.
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция принимает число введенное пользователем, количество символов, и выводит третью цифру числа. Если 3 цифры нет, сообщает и выводит 0.
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.Write("Третьей цифры нет, держи: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
         }
 
-
+        result = (a / c) % 10;
     }
+return result;
+}
