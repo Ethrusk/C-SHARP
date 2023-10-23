554283e7a81b9bbe541fcadd011ee3b0afb2be9d
Ethrusk <ethrusk@yandex.ru>
2023-10-23 15:41:27 +0300

    Seminar 02 - Task 10

---
+++ C-SHARP SEMINARS/Seminar 02 - Task 10/Program.cs
@@ -0,0 +1,25 @@
+﻿// **Задача 10:** Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает **вторую** цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.WriteLine("Введите любое трёхзначное число и программа отобразит вторую цифру этого числа");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введённое вами число ");
+Console.Write(number);
+
+Console.WriteLine(" ");
+
+if (100 < number && number < 1000)
+{
+    int SecondNumber = number % 100 / 10;
+    Console.Write("Вторая цифра введённого вами числа - ");
+    Console.Write(SecondNumber);
+}
+else
+{
+    Console.WriteLine("Введите число меньше 1000");
+}
