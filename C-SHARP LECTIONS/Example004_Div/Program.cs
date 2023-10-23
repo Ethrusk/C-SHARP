4215f78144139be3db2a46815a6c40b0f27e59fd
Ethrusk <ethrusk@yandex.ru>
2023-10-23 14:57:31 +0300

    Seminar 01 - Task 04

---
+++ C-SHARP LECTIONS/Example004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿// Тренировка Example004_Div
+double numberA = 18;
+double numberB = 7;
+Console.WriteLine(numberA / numberB);
+
---
+++ C-SHARP SEMINARS/Seminar 01 - Task 04/Program.cs
@@ -0,0 +1,29 @@
+﻿// ### Задача 4:
+// Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Задача №4 - определение максимального числа из трёх чисел");
+
+Console.Write("Введите первое число: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumberA);
+
+Console.Write("Введите второе число: ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumberB);
+
+Console.Write("Введите третье число: ");
+int NumberС = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumberС);
+
+int max=0;
+
+if (NumberA > max) max = NumberA;
+if (NumberB > max) max = NumberB;
+if (NumberС > max) max = NumberС;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
