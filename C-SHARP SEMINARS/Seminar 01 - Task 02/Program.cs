fe293518dda6767f5f7eb27c2ab6b95fb19dba65
Ethrusk <ethrusk@yandex.ru>
2023-10-23 14:40:34 +0300

    Seminar 01 - Task 02

---
+++ C-SHARP SEMINARS/Seminar 01 - Task 02/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая на вход принимает
+// два числа и выдает, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Задача № 2 - определение большего числа из двух");
+Console.Write("Введите первое число: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumberA);
+Console.Write("Введите второе число: ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumberB);
+
+int max = 0;
+
+if (NumberA > NumberB)
+{
+    max = NumberA;
+    Console.Write("Max = ");
+    Console.WriteLine(max);
+    Console.Write("Первое число больше второго числа");
+}
+else 
+{
+    max = NumberB;
+    Console.Write("Max = ");
+    Console.WriteLine(max);
+    Console.Write("Второе число больше первого числа");
+}
