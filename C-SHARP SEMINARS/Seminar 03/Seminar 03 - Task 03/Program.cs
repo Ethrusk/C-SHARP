6c16f661add9c804e3127720c3f3924e26418440
Ethrusk <ethrusk@yandex.ru>
2023-10-26 14:59:05 +0300

    Seminar 03 - Task 03 - code done

--- C-SHARP SEMINARS/Seminar 03/Seminar 03 - Task 03/Program.cs
+++ C-SHARP SEMINARS/Seminar 03/Seminar 03 - Task 03/Program.cs
@@ -6,18 +6,22 @@
 Console.WriteLine("Задача 03 - нахождение значений в отрезке [20, 90] для массива из 10 целых чисел");
 
 int[] array = new int[10];
-bool inSpase = false;
 
+Console.WriteLine("Получен массив:");
 for(int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(1, 101);
-    Console.Write(array[i] + "; ");     
-}
-
-Console.WriteLine(" ");   
-
-if(array[i] > 20 && array[i] < 90)
-{
-    inSpase = true;
     Console.Write(array[i] + "; ");
+}        
+
+int count = 0;
+
+Console.WriteLine(" ");
+Console.WriteLine("Количество значений в отрезке от 20 до 90: ");
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 20 && array[i] < 90)
+    count++;
 }
+
+Console.Write(count);
