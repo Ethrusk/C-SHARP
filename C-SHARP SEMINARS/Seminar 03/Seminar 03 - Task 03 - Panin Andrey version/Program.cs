1965644c9986db57a97ed640a012ecb1ad6fd2c5
Ethrusk <ethrusk@yandex.ru>
2023-10-27 10:54:17 +0300

    Seminar 03 - Task 03 - Panin Andrey version

---
+++ C-SHARP SEMINARS/Seminar 03/Seminar 03 - Task 03 - Panin Andrey version/Program.cs
@@ -0,0 +1,23 @@
+﻿double[] array = new double[10];
+for(int i = 0; i < array.Length; i++)
+{
+    double a = new Random().Next(1, 100);
+    double b = new Random().NextDouble();
+    array[i] = Math.Round(a + b, 2);
+    Console.Write(array[i] + " ");
+}
+double max = array[0];
+double min = array[0];
+for(int i = 0; i< array.Length; i++)
+{
+    if(array[i] > max)
+    {
+        max = array[i];
+    }
+    else if(array[i] < min)
+    {
+        min = array[i];
+    }
+}
+double result = max - min;
+Console.Write($"=> {result}");
