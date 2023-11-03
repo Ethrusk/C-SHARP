ba407dfcdfad7b004254b85a3f0b498513b2d579
Ethrusk <ethrusk@yandex.ru>
2023-11-03 11:17:39 +0300

    Seminar 04 - Task 25 var2 code described

--- C-SHARP SEMINARS/Seminar 04/Seminar 04 - Task 25 var2/Program.cs
+++ C-SHARP SEMINARS/Seminar 04/Seminar 04 - Task 25 var2/Program.cs
@@ -6,12 +6,23 @@
 // 2. Использовать метод Math.Pow(A,Б) для возведения числа А в натуральную степень B 
 
 
-void numberAB(double a , double b)
-{
-    
-    while(a < 10 && b < 10)
+void numberAB(double a , double b) // Понятие метода. Примеры методов в классах. Возврат из метода. 
+                                   // Оператор return. Методы без параметров. Ключевое слово void
+                                   // https://www.bestprog.net/ru/2018/10/25/the-concept-of-the-method-examples-of-methods-in-classes-return-from-method-the-return-statement-methods-without-parameters-the-void-keyword_ru/
+                                   // Если метод ничего не возвращает, то вместо типа указывается слово void
+                                        // void MethodName(parameters_list)
+                                        // {
+                                        //     // ...
+                                        // }
+                                        // где parameter_list – список параметров метода с именем MethodName, которые он получает.
+
+
+{    
+    while(a < 10 && b < 10) // этот цикл возводит числа в степень, начиная от введённых с консоли
+                            // и заканчивая числом меньше 10 (9)
+                            // Если хотим возведения в степнеь только одного числа, то нужно убрать цикл с шагами
     {
-        double result = Math.Pow(a,b);
+        double result = Math.Pow(a,b); // Math.Pow Возвращает указанное число, возведенное в указанную степень.
         Console.WriteLine($" {a} ^ {b} = {result}");
         a++;
         b++;
