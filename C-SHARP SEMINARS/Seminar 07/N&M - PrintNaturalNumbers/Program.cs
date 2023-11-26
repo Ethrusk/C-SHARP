a89e8052afab39ab82218f94e31d4fce1ab27803
Mila <147173617+Ethrusk@users.noreply.github.com>
2023-11-26 22:39:43 +0300

    Natural Numbers.md

--- C-SHARP SEMINARS/Seminar 07/N&M - PrintNaturalNumbers/Program.cs
+++ C-SHARP SEMINARS/Seminar 07/N&M - PrintNaturalNumbers/Program.cs
@@ -24,3 +24,20 @@ void PrintNaturalNumbers(int m, int n)
 }
 
 PrintNaturalNumbers(m,n);
+
+
+// иначе через цикл
+
+// Задать значение N и M. Создать программу, которая выведет все натуральные числа в промежутке от N до M
+
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"N={N}, M={M}");
+if (M>N)
+for (int i = N; i <= M; i++)
+    Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+        Console.Write($" {i}");
