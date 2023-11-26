7d15d84a8c53dcf0ad8ec23918444f56dc51ef79
Mila <147173617+Ethrusk@users.noreply.github.com>
2023-11-26 22:35:17 +0300

    find sum code added

--- C-SHARP SEMINARS/Seminar 07/N&M - PrintNaturalNumbers/Program.cs
+++ C-SHARP SEMINARS/Seminar 07/N&M - PrintNaturalNumbers/Program.cs
@@ -1,18 +1,24 @@
 ﻿
 // Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
 // Использовать рекурсию
+// Исключить отрицательные числа и 0
 
 Console.WriteLine("Введите значение M: ");
-int m = Convert.ToInt32(Console.ReadLine());
+double m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите значение N: ");
-int n = Convert.ToInt32(Console.ReadLine());
+double n = Convert.ToInt32(Console.ReadLine());
+
+if (m <= 0 || n <= 0)
+{
+    Console.WriteLine("Введите целое положительное число");
+}
 
 void PrintNaturalNumbers(int m, int n)
 {
     if (m > n)
-    return;
+    {return;}
 
-    Console.Write($"{m}, ");
+    Console.Write($"{m}  ");
     PrintNaturalNumbers(m+1, n);
     Console.WriteLine();
 }
---
+++ C-SHARP SEMINARS/Seminar 07/N&M - find sum/Program.cs
@@ -0,0 +1,46 @@
+﻿// Через рекурсию напишите программу, которая находит сумму натуральных элементов в промежутке от M до N
+
+Volga_, Вот код со скрином:
+
+Console.WriteLine("Чтобы узнать сумму натуральных чисел между числами M и N, введите целое положительное число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чтобы узнать сумму натуральных чисел между числами M и N, введите целое положительное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+ 
+int SumNumbers(int M, int N)
+{
+    if (M == 0) return (N * (N + 1)) / 2;            // Если M равно нулю
+            else if (N == 0) return (M * (M + 1)) / 2;       // Если N равно нулю
+            else if (M == N) return M;                       // Если M=N
+            else if (M < N) return N + SumNumbers(M, N - 1); // Если M<N
+            else return N + SumNumbers(M, N + 1);            // Если M>N
+}
+void Main(string[] args)
+{
+    Console.Write("M = ");
+    int M = int.Parse(Console.ReadLine());
+    Console.Write("N = ");
+    int N = int.Parse(Console.ReadLine());
+    Console.WriteLine($"Result, S = {SumNumbers(M, N)}");
+    Console.ReadLine();
+}
+
+
+// иначе
+
+
+Console.Write("M = ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("N = ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"Result, S = {SumNumbers(M, N)}");
+Console.ReadLine();
+ 
+static int SumNumbers(int M, int N)
+{
+    if (M == 0) return (N * (N + 1)) / 2;            // Если M равно нулю
+    else if (N == 0) return (M * (M + 1)) / 2;       // Если N равно нулю
+    else if (M == N) return M;                       // Если M=N
+    else if (M < N) return N + SumNumbers(M, N - 1); // Если M<N
+    else return N + SumNumbers(M, N + 1);            // Если M>N
+}
