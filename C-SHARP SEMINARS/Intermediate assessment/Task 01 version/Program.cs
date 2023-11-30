bb6561e52a8bf6856296d7f2b4cf891ce1b3d9bb
Ethrusk <ethrusk@yandex.ru>
2023-11-30 17:18:48 +0300

    Intermediate assessment Task 01 added

---
+++ C-SHARP SEMINARS/Intermediate assessment/Task 01 version/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Метод проверки числа на натуральность
+
+
+
+Console.WriteLine("Введите значения M и N:");
+        int m = Convert.ToInt32(Console.ReadLine());
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        while (m <= 0 || n <=0)
+        {
+            Console.WriteLine("Оба числа должны быть натуральными.");
+            Console.WriteLine("Введите значения M и N:");
+            m = Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+        }
+
+        PrintNaturalNumbers(m, n);
+    
+
+    void PrintNaturalNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.Write(m + " ");
+            PrintNaturalNumbers(m + 1, n);
+        }
+    }
+
+bool IsNaturalNumber(int number)
+    {
+        return !(number % 1 == 0);
+    }
---
+++ C-SHARP SEMINARS/Intermediate assessment/Task 01/Program.cs
@@ -0,0 +1,37 @@
+﻿// **Задача 1**
+
+// Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+
+
+
+Console.WriteLine("Задача № 1 - выведение в консоль всех натуральных чисел от M до N");
+
+void PrintNaturalNumbers(int m, int n) // Рекурсивный метод вывода на печать чисел в диапазоне от M до N
+{
+    if (m > n)
+    {return;}
+
+    Console.Write($"{m}  ");
+    PrintNaturalNumbers( m + 1, n);
+}
+
+
+Console.WriteLine("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while (m <= 0 || n <=0) // Проверка чисел на натуральность
+        {
+            Console.WriteLine("Оба числа должны быть натуральными.");
+            Console.WriteLine("Введите значение M: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение N: ");
+            n = Convert.ToInt32(Console.ReadLine());
+        }
+
+Console.WriteLine("Диапазон значений от M до N: ");
+PrintNaturalNumbers(m, n);
