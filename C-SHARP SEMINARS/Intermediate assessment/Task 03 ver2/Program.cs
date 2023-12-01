858f895defe0db6c64aca4a9bd3ca5fe7298e7ce
Mila <147173617+Ethrusk@users.noreply.github.com>
2023-12-01 22:37:04 +0300

    Intermediate assessment tasks codes done

--- C-SHARP SEMINARS/Intermediate assessment/Task 03 ver2/Program.cs
+++ C-SHARP SEMINARS/Intermediate assessment/Task 03 ver2/Program.cs
@@ -1,24 +1,31 @@
 ﻿// **Задача 3**
 
 // Задайте произвольный массив. 
-// Например {1, 2, 5, 0, 10, 34} => {34, 10, 0, 5, 2, 1}
 // Выведете его элементы, начиная с конца. 
 // Использовать рекурсию, не использовать циклы.
 
 
 Console.WriteLine("Задача № 3 - выведение в консоль элементов произвольного  массива с помощью рекурсии");
 
-Console.WriteLine("\nИмеем массив {1, 2, 5, 0, 10, 34}");
 
-void PrintReverseArray(int [] MyArray, int i = 0)
+// Создаём массив из 10 элементов, заполняем его произвольными числами и выводим в консоль
+
+int[] MyArray = new int[10];
+
+Console.WriteLine("Получен массив:");
+for(int i = 0; i < MyArray.Length; i++)
+{
+    MyArray[i] = new Random().Next(1, 101);
+    Console.Write(MyArray[i] + ", ");
+}
+
+void PrintReverseMyArray(int [] MyArray, int i = 0)
         {
             if (i < MyArray.Length)
-            {PrintReverseArray(MyArray, i+1);
+            {PrintReverseMyArray(MyArray, i+1);
             Console.Write(MyArray[i] + ", ");}
         }
 
-int[] MyArray = {1, 2, 5, 0, 10, 34};
+Console.WriteLine(" ");
 Console.WriteLine("Элементы массива в обратном порядке: ");
-Console.Write("{");
-PrintReverseArray(MyArray);
-Console.Write("}");
+PrintReverseMyArray(MyArray);
---
+++ C-SHARP SEMINARS/Seminar 07/Task 03 ver3/Program.cs
@@ -0,0 +1,43 @@
+﻿// **Задача 3**
+
+// Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+
+Console.WriteLine("Задача № 3 - выведение в консоль элементов произвольного  массива с помощью рекурсии");
+
+
+// Создаём массив из произвольного количества элементов, заданного с клавиатуры, заполняем его также данными с клавитауры
+
+
+Console.Write("Введите число для размера массива - ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] MyArray = new int[num];
+
+for(int i = 0; i < MyArray.Length; i++)
+{
+    // Console.Write("Введите числа для заполнения массива - ");
+    Console.WriteLine("Введите число {0}:", i + 1);
+    MyArray[i] = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\n Получен массив:");
+        foreach (var item in MyArray)
+        {
+            Console.Write(i + " ");
+        }
+
+    // Console.WriteLine("Получен массив:");
+    // Console.Write(MyArray[i] + ", ");
+}
+
+void PrintReverseMyArray(int [] MyArray, int i = 0)
+        {
+            if (i < MyArray.Length)
+            {PrintReverseMyArray(MyArray, i+1);
+            Console.Write(MyArray[i] + ", ");}
+        }
+
+Console.WriteLine(" ");
+Console.WriteLine("Элементы массива в обратном порядке: ");
+PrintReverseMyArray(MyArray);
